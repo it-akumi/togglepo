91c155b8bd05773a9671f38c845b10d816b0e572
it_akumi <it.akumi1011@gmail.com>
2018-01-30 16:34:15 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='togglepo',
+    version='0.0',
+    description='Togglepo shows how much you achieve your goal.',
+    author='Takumi Ishii',
+    install_requires=[
+        'click'
+    ],
+    url='https://github.com/it-akumi/togglepo',
+    entry_points = {
+        'console_scripts': [
+            'tglp = tglp.main:main'
+        ]
+    }
+)
