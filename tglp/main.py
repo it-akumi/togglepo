4537835627ba59aca10e06929f4b84457dc24e41
it_akumi <it.akumi1011@gmail.com>
2018-02-06 11:56:33 +0900

    Add normalize_second

--- tglp/main.py
+++ tglp/main.py
@@ -8,19 +8,23 @@ from tglp.config import Parser
 from tglp.period import Period
 
 
+def normalize_second(seconds):
+    """Normalize second to '%H-%M-%S'."""
+    m, s = map(int, divmod(seconds, 60))
+    h, m = map(int, divmod(m, 60))
+    return '{:5}h {:2}m {:3}s'.format(h, m, s)
+
+
 @click.command()
 def main():
     """Togglepo shows how much you achieve your goals."""
     conf = Parser().parse_json_config()
     api = TogglAPI(conf.get('API_TOKEN'), conf.get('WORKSPACE_ID'))
-    period = Period(conf.get('SINCE'), '2020-01-01')
+    period = Period(conf.get('SINCE'), conf.get('UNTIL'))
 
     table = PrettyTable(['Project', 'Achieved'])
     for prj, time_sec in api.total_time_entries(period.divide_passed_period()).items():
-        hours = int(time_sec / 3600)
-        minutes = int((time_sec - hours * 3600) / 60)
-        seconds = time_sec % 60
-        achieved = "{:5}h {:2}m {:3}s".format(hours, minutes, seconds)
+        achieved = normalize_second(time_sec)
         table.add_row([prj, achieved])
     print(table)
 
