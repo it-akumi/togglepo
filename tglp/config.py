1756dccb301c623b87bcbd03af067e1756d0e819
it_akumi <it.akumi1011@gmail.com>
2018-02-09 07:37:25 +0900

    Make sure to exist config before open

--- tglp/config.py
+++ tglp/config.py
@@ -2,12 +2,17 @@
 """A handler of config."""
 import json
 import os
+import sys
 
 
 class Config:
     """A handler of config."""
 
-    def __init__(self, config_file_path='~/.tglp.json'):
-        """Load config."""
-        with open(os.path.expanduser(config_file_path), 'r') as config:
+    def __init__(self, config_file='~/.tglp.json'):
+        """Load config if it exists."""
+        config_file_path = os.path.expanduser(config_file)
+        if not os.path.exists(config_file_path):
+            sys.stderr.write('No config found\n')
+            sys.exit(1)
+        with open(config_file_path, 'r') as config:
             self.config = json.load(config)
