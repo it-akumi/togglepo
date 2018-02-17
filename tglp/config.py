1be610bd2f02fe144fd5abda22858648b33a77e2
it_akumi <it.akumi1011@gmail.com>
2018-02-17 17:18:36 +0900

    Use pathlib instead of os.path

--- tglp/config.py
+++ tglp/config.py
@@ -1,8 +1,8 @@
 # coding:utf-8
 """A handler of config."""
 import json
-import os
 import sys
+from pathlib import Path
 
 
 class Config:
@@ -10,9 +10,9 @@ class Config:
 
     def __init__(self, config_file):
         """Load config if it exists."""
-        config_file_path = os.path.expanduser(config_file)
-        if not os.path.exists(config_file_path):
+        config_file_path = Path(config_file).expanduser()
+        if not config_file_path.exists():
             sys.stderr.write('No config found\n')
             sys.exit(1)
-        with open(config_file_path, 'r') as config:
+        with config_file_path.open() as config:
             self.config = json.load(config)
